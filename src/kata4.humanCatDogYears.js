a2e2ae044ff3bfe541a2939486f389abf4f31db4
Daniel Cross <34304333+Daniel-Cross@users.noreply.github.com>
2019-02-16 16:14:04 +0000

    Update kata4.humanCatDogYears.js

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,7 @@
+//Many different ways to do this one but I like your attempt at keeping it fairly clear and concise. Also, good use of a while loop, which you don't see often.
+//However, if the human years number entered was 0 or a minus number, this code wouldn't run as intended. Your results would be [31, 140, 169] instead of [0, 0, 0].
+//Always a good idea to have a failsafe incase the code isn't used for the intended purpose or a user enters a wrong value.
+//Very good effort.
 const humanCatDogYears = (number) => {
     let count = 0;
     let dogYears = 0;
